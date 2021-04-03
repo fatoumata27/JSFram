ef06b6fe6a3d6d83a6ddea680a6b058686f83377
koala819 <x.genolhac@gmail.com>
2021-04-03 12:17:00 +0200

    Create first EventListener

--- Frontend/scripts/register.js
+++ Frontend/scripts/register.js
@@ -1,1 +1,7 @@
-alert ("hello");
+const registerForm = document.getElementById("registerForm");
+
+registerForm.addEventListener("submit", (e) => {
+    console.log(e);
+    alert("hello");
+    e.preventDefault();
+})
