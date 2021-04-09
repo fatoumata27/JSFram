6aab26f0d9bb12634ea5c67dda6af95c55e4de7b
koala819 <x.genolhac@gmail.com>
2021-04-09 09:33:06 +0200

    create first field missing firstname

--- Frontend/scripts/register.js
+++ Frontend/scripts/register.js
@@ -1,7 +1,31 @@
+/*
+const lastname = document.getElementById('lastname');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const send_button = document.getElementById('send_button');
+const form = document.getElementById('registerForm');
+let fieldOK = true;
+console.log('je suis là');
+form.addEventListener('submit', (e) => {
+    console.log(e);
+    alert('hello');
+    if (firstname.validity.valueMissing) {
+        alert('bou');
+        //preventDefault <=> Arret du comportement normal du HTML
+        e.preventDefault();
+        missfirstname.textContent = 'Nom manquant';
+        missfirstname.style.color = 'red';
+        fieldOK = false;
+    }
+})*/
+
+const firstname = document.getElementById('firstname');
 const registerForm = document.getElementById("registerForm");
 
 registerForm.addEventListener("submit", (e) => {
-    console.log(e);
-    alert("hello");
-    e.preventDefault();
-})
+    if (!firstname.validity.valid) {
+        e.preventDefault();
+        missfirstname.textContent = 'Nom manquant';
+        missfirstname.style.color = 'red';
+    }
+});
